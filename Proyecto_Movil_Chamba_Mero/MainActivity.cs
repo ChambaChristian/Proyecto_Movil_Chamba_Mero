f3b0938fb3d4e87f12bcdad412b7d53ce7f35cb0
CHRISTIAN ALEXANDER CHAMBA CLAVIJO <christian.chamba.clavijo@estudiantes.cordillera.edu.ec>
2021-06-13 16:47:41 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto_Movil_Chamba_Mero/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Android.Widget;
+using System;
+using Android.Content;
+
+namespace Proyecto_Movil_Chamba_Mero
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            //var registrar = FindViewById<TextView>(Resource.Id.txt_regis);
+            //registrar.Click += Txt_Click;
+            SetContentView(Resource.Layout.activity_main);
+
+        }
+
+        //private void Txt_Click(object sender, EventArgs e)
+        //{
+        //    Intent intent = new Intent(Intent.ActionView, Android.Net.Uri.Parse("http://wwww.google.com"));
+        //    StartActivity(intent);
+        //}
+        //Proyecto_Movil_Chamba_Mero
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
